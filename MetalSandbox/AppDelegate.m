a92192c499987860aa4edd51a10f50b31e7b6403
Chinmay Garde <chinmaygarde@gmail.com>
2025-04-12 10:14:04 -0700

    Initial Commit

---
+++ MetalSandbox/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  MetalSandbox
+//
+//  Created by Chinmay Garde on 4/12/25.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
+
+@end
---
+++ MetalSandbox/Renderer.h
@@ -0,0 +1,17 @@
+//
+//  Renderer.h
+//  MetalSandbox
+//
+//  Created by Chinmay Garde on 4/12/25.
+//
+
+#import <MetalKit/MetalKit.h>
+
+// Our platform independent renderer class.   Implements the MTKViewDelegate protocol which
+//   allows it to accept per-frame update and drawable resize callbacks.
+@interface Renderer : NSObject <MTKViewDelegate>
+
+-(nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)view;
+
+@end
+
